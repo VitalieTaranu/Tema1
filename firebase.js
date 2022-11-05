c77ae760db7d302deb4d70fb1d3f97ae2e87929a
VitalieTaranu <109237007+VitalieTaranu@users.noreply.github.com>
2022-11-06 01:23:38 +0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC1GF5K_VYIfn17ZT9oz5f_iOMTzhb-cao",
+    authDomain: "project-topfilm.firebaseapp.com",
+    projectId: "project-topfilm",
+    storageBucket: "project-topfilm.appspot.com",
+    messagingSenderId: "578916720661",
+    appId: "1:578916720661:web:cae624be932fd834806e1c"
+  };
